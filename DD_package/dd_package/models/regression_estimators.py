1005c518d05565345265998738afbde9917ace99
sorooshi <sr.shalileh@gmail.com>
2022-02-22 01:25:54 +0300

    remove predict_proba methos

--- DD_package/dd_package/models/regression_estimators.py
+++ DD_package/dd_package/models/regression_estimators.py
@@ -335,13 +335,11 @@ class RegressionEstimators:
             y_test = v["y_test"]
             x_test = v["x_test"]
             y_pred = self.estimator.predict(x_test)
-            y_pred_prob = self.estimator.predict_proba(x_test)
             end = time.time()
 
             self.results[k]["y_test"] = y_test
             self.results[k]["x_test"] = x_test
             self.results[k]["y_pred"] = y_pred
-            self.results[k]["y_pred_prob"] = y_pred_prob
             self.results[k]["exe_time"] = end - start
 
             run = util.wandb_metrics(
